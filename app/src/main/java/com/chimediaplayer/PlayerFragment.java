16fc859a1185c48546a1f13030eed117070f0bf0
Pavel Vinnik <vinnik.pavel@gmail.com>
2021-02-17 16:56:22 +0200

    Hardcoded track to play. Added buttons to control. Added strings.xml for control text.

--- app/src/main/java/com/chimediaplayer/PlayerFragment.java
+++ app/src/main/java/com/chimediaplayer/PlayerFragment.java
@@ -1,9 +1,12 @@
 package com.chimediaplayer;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +14,54 @@ import androidx.fragment.app.Fragment;
 
 public class PlayerFragment extends Fragment {
 
+    private Context mContext;
+    private Intent mPlayerServiceIntent;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPlayerServiceIntent = new Intent(getContext(), PlayerService.class);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_player, container, false);
+        Button playButton = view.findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayerServiceIntent.setAction(PlayerService.PLAY_ACTION);
+                getContext().startService(mPlayerServiceIntent);
+            }
+        });
+        Button pauseButton = view.findViewById(R.id.pauseButton);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPlayerServiceIntent.setAction(PlayerService.PAUSE_ACTION);
+                getContext().startService(mPlayerServiceIntent);
+            }
+        });
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mContext = null;
     }
 
     public static PlayerFragment newInstance() {
--- app/src/main/java/com/chimediaplayer/PlayerService.java
+++ app/src/main/java/com/chimediaplayer/PlayerService.java
@@ -2,14 +2,61 @@ package com.chimediaplayer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 
 public class PlayerService extends Service {
+
+    public static final String PLAY_ACTION = "playAction";
+    public static final String PAUSE_ACTION = "pauseAction";
+    public static final String STOP_ACTION = "stopAction";
+    public static final String NEXT_ACTION = "nextAction";
+    public static final String PREVIOUS_ACTION = "previousAction";
+
+    private MediaPlayer mMediaPlayer;
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.dance_macabre);
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        switch (intent.getAction()) {
+            case PLAY_ACTION: {
+                mMediaPlayer.start();
+                break;
+            }
+            case PAUSE_ACTION: {
+                mMediaPlayer.pause();
+                break;
+            }
+            case STOP_ACTION: {
+                break;
+            }
+            case NEXT_ACTION: {
+                break;
+            }
+            case PREVIOUS_ACTION: {
+                break;
+            }
+        }
+        return START_NOT_STICKY;
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
